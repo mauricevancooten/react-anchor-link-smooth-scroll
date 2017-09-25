67d7d686bd55c21144e300e4e1862229621eafd0
Maurice van Cooten <maurice@minus.nz>
2017-09-25 13:02:26 +1300

    resolve src

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ const config = {
       }
     ]
   },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  },
   externals: {
     react: 'react'
   },

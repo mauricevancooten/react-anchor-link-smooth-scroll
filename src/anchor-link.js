7bdb8d6b6986369db6a332057a2755eb9c705668
Maurice van Cooten <maurice@minus.nz>
2017-09-27 20:44:34 +1300

    parse all props for when adding a class name and added offset for sticky navigation

--- src/anchor-link.js
+++ src/anchor-link.js
@@ -8,17 +8,20 @@ class AnchorLink extends Component {
   componentDidMount() {
     require('smoothscroll-polyfill').polyfill()
   }
-  smoothScroll(e) {
+  smoothScroll(e, offset = 0) {
     e.preventDefault()
+    if (typeof this.props.offset !== 'undefined') {
+      offset = parseInt(this.props.offset)
+    }
     const id = e.target.getAttribute('href').slice(1)
     window.scroll({
-      top: document.getElementById(`${id}`).offsetTop,
+      top: document.getElementById(`${id}`).offsetTop - offset,
       behavior: 'smooth'
     })
   }
   render() {
     return (
-      <a href={this.props.href} onClick={this.smoothScroll}>{this.props.children}</a>
+      <a {...this.props} onClick={this.smoothScroll}>{this.props.children}</a>
     )
   }
 }

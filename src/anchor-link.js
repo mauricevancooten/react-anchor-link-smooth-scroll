dcf59de9d4366f47a2073247eafed9cef16a74ba
Kyle Sosnowski <ktsosno@gmail.com>
2019-05-13 22:10:40 -0700

    Prevent undefined exception on missing ID

--- src/anchor-link.js
+++ src/anchor-link.js
@@ -20,11 +20,15 @@ class AnchorLink extends Component {
     }
     const id = e.currentTarget.getAttribute('href').slice(1)
     const $anchor = document.getElementById(id);
-    const offsetTop = $anchor.getBoundingClientRect().top + window.pageYOffset;
-    window.scroll({
-      top: offsetTop - offset(),
-      behavior: 'smooth'
-    })
+
+    if ($anchor) {
+      const offsetTop = $anchor.getBoundingClientRect().top + window.pageYOffset;
+      window.scroll({
+        top: offsetTop - offset(),
+        behavior: 'smooth'
+      })
+    }
+
     if (this.props.onClick) {this.props.onClick(e)}
   }
   render() {

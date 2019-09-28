cc8839c2b1ae3124560b2669bd101c20effc90c6
John O. Paul <oyomthojohnpaul@gmail.com>
2019-09-28 13:31:58 +0300

    Fix collision with 'hashchange' event handlers

--- src/anchor-link.js
+++ src/anchor-link.js
@@ -8,24 +8,32 @@ class AnchorLink extends Component {
   componentDidMount() {
     require('smoothscroll-polyfill').polyfill()
   }
-  smoothScroll(e) {
-    e.preventDefault()
-    let offset = () => 0
-    if (typeof this.props.offset !== 'undefined') {
-      if (!!(this.props.offset && this.props.offset.constructor && this.props.offset.apply)) {
-        offset = this.props.offset
-      } else {
-        offset = () => parseInt(this.props.offset)
-      }
+  smoothScroll(event) {
+    event.preventDefault()
+    const e = { ...event }
+    const { href } = this.props;
+    if (history.pushState && href) {
+      history.pushState({}, '', href)
+      window.dispatchEvent(new Event('hashchange'))
     }
-    const id = e.currentTarget.getAttribute('href').slice(1)
-    const $anchor = document.getElementById(id);
-    const offsetTop = $anchor.getBoundingClientRect().top + window.pageYOffset;
-    window.scroll({
-      top: offsetTop - offset(),
-      behavior: 'smooth'
-    })
-    if (this.props.onClick) {this.props.onClick(e)}
+    setTimeout(() => {
+      let offset = () => 0
+      if (typeof this.props.offset !== 'undefined') {
+        if (!!(this.props.offset && this.props.offset.constructor && this.props.offset.apply)) {
+          offset = this.props.offset
+        } else {
+          offset = () => parseInt(this.props.offset)
+        }
+      }
+      const id = e.currentTarget.getAttribute('href').slice(1)
+      const $anchor = document.getElementById(id);
+      const offsetTop = $anchor.getBoundingClientRect().top + window.pageYOffset;
+      window.scroll({
+        top: offsetTop - offset(),
+        behavior: 'smooth'
+      })
+      if (this.props.onClick) {this.props.onClick(e)}
+    }, 0);
   }
   render() {
     const { offset, ...rest } = this.props;
